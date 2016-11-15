b75f188ebeb051e7d49bbf318eb9f565f4951505
jbsempe <jeanbaptiste.sempe@gmail.com>
2016-11-15 15:29:12 +0100

    Add boolean and migration for users role

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   #Devise(Gestion utilisateurs)
   devise_for :users,
+             controllers: { registrations: 'users/registrations' },
              class_name: 'User',
              path: '/',
-             path_names: { sign_in: 'connexion' }
+             path_names: { sign_in: 'connexion', sign_up: 'inscription' }
 
   root to: 'static_pages#home'
 
